8dbbca4353688af7527ce76db3f751aed707da46
Wissam Al-Wakeel <wissam.alwakeel@gmail.com>
2017-12-16 16:52:20 +0100

    Refactor MST to read graph from system in

--- src/main/java/search/search/MSTsearch.java
+++ src/main/java/search/search/MSTsearch.java
@@ -1,107 +1,85 @@
 package search.search;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.commons.lang3.StringUtils;
+
 public class MSTsearch {
-	private static final int V=5;
-	int minKey(int key[], Boolean mstSet[])
-    {
-        // Initialize min value
-        int min = Integer.MAX_VALUE, min_index=-1;
- 
-        for (int v = 0; v < V; v++)
-            if (mstSet[v] == false && key[v] < min)
-            {
-                min = key[v];
-                min_index = v;
-            }
- 
-        return min_index;
-    }
- 
-    // A utility function to print the constructed MST stored in
-    // parent[]
-    void printMST(int parent[], int n, int graph[][])
-    {
-        System.out.println("Edge   Weight");
-        for (int i = 1; i < V; i++)
-            System.out.println(parent[i]+" - "+ i+"    "+
-                               graph[i][parent[i]]);
-    }
- 
-    // Function to construct and print MST for a graph represented
-    //  using adjacency matrix representation
-    void primMST(int graph[][])
-    {
-        // Array to store constructed MST
-        int parent[] = new int[V];
- 
-        // Key values used to pick minimum weight edge in cut
-        int key[] = new int [V];
- 
-        // To represent set of vertices not yet included in MST
-        Boolean mstSet[] = new Boolean[V];
- 
-        // Initialize all keys as INFINITE
-        for (int i = 0; i < V; i++)
-        {
-            key[i] = Integer.MAX_VALUE;
-            mstSet[i] = false;
-        }
- 
-        // Always include first 1st vertex in MST.
-        key[0] = 0;     // Make key 0 so that this vertex is
-                        // picked as first vertex
-        parent[0] = -1; // First node is always root of MST
- 
-        // The MST will have V vertices
-        for (int count = 0; count < V-1; count++)
-        {
-            // Pick thd minimum key vertex from the set of vertices
-            // not yet included in MST
-            int u = minKey(key, mstSet);
- 
-            // Add the picked vertex to the MST Set
-            mstSet[u] = true;
- 
-            // Update key value and parent index of the adjacent
-            // vertices of the picked vertex. Consider only those
-            // vertices which are not yet included in MST
-            for (int v = 0; v < V; v++)
- 
-                // graph[u][v] is non zero only for adjacent vertices of m
-                // mstSet[v] is false for vertices not yet included in MST
-                // Update the key only if graph[u][v] is smaller than key[v]
-                if (graph[u][v]!=0 && mstSet[v] == false &&
-                    graph[u][v] <  key[v])
-                {
-                    parent[v]  = u;
-                    key[v] = graph[u][v];
-                }
-        }
- 
-        // print the constructed MST
-        printMST(parent, V, graph);
-    }
-    public static void mst()
-    {
-        /* Let us create the following graph
-           2    3
-        (0)--(1)--(2)
-        |    / \   |
-        6| 8/   \5 |7
-        | /      \ |
-        (3)-------(4)
-             9          */
-        MSTsearch t = new MSTsearch();
-        int graph[][] = new int[][] {{0, 2, 0, 6, 0},
-                                    {2, 0, 3, 8, 5},
-                                    {0, 3, 0, 0, 7},
-                                    {6, 8, 0, 0, 9},
-                                    {0, 5, 7, 9, 0},
-                                   };
- 
-        // Print the solution
-        t.primMST(graph);
-    }
+	private static int vertices = 5;
+
+	int getMinmumKey(int key[], Boolean mstSet[]) {
+		int minimum = Integer.MAX_VALUE, minmumIndex = -1;
+		for (int v = 0; v < vertices; v++)
+			if (mstSet[v] == false && key[v] < minimum) {
+				minimum = key[v];
+				minmumIndex = v;
+			}
+		return minmumIndex;
+	}
+
+	void printMST(int parent[], int n, int graph[][]) {
+		System.out.println("Edge   Weight");
+		for (int i = 1; i < vertices; i++)
+			System.out.println(parent[i] + " - " + i + "    " + graph[i][parent[i]]);
+	}
+
+	void primMST(int graph[][]) {
+		int parent[] = new int[vertices];
+		int key[] = new int[vertices];
+		Boolean mstSet[] = new Boolean[vertices];
+		for (int i = 0; i < vertices; i++) {
+			key[i] = Integer.MAX_VALUE;
+			mstSet[i] = false;
+		}
+
+		key[0] = 0; 
+		parent[0] = -1; 
+		for (int count = 0; count < vertices - 1; count++) {
+			int u = getMinmumKey(key, mstSet);
+			mstSet[u] = true;
+			for (int v = 0; v < vertices; v++)
+				if (graph[u][v] != 0 && mstSet[v] == false && graph[u][v] < key[v]) {
+					parent[v] = u;
+					key[v] = graph[u][v];
+				}
+		}
+		printMST(parent, vertices, graph);
+	}
+
+	public static void mst() { 
+		MSTsearch t = new MSTsearch();
+		int graph[][] = t.buildGraph();
+// Test Data = { 0, 2, 0, 6, 0 }, { 2, 0, 3, 8, 5 }, { 0, 3, 0, 0, 7 }, { 6, 8, 0, 0, 9 },{ 0, 5, 7, 9, 0 }
+		t.primMST(graph);
+	}
+	
+	private int[][] buildGraph() {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		int graph[][] = new int[vertices][];
+		try {
+			System.out.println("Enter the number of vertices (integer expected): ");
+			vertices = Integer.parseInt(reader.readLine());
+			for (int i = 0; i < vertices; i++) { 
+				System.out.println("Enter Graph data in format i,i1,i2,...in " + (i+1));
+				reader = new BufferedReader(new InputStreamReader(System.in));
+				String graphData = reader.readLine();
+				graph[i] = parseData(graphData);
+			}			
+		} catch (IOException e) {
+			System.out.println("Unable to build graph duo to wrong input");
+		}		
+		return graph;
+	}
+
+	private int[] parseData(String graphData) {
+		graphData = StringUtils.remove(graphData, " ");
+		String graphDataArray[] = graphData.split(",");
+		int[] edgeData = new int[vertices];
+		for (int x = 0; x < vertices; x++) {
+			edgeData[x] = Integer.parseInt(graphDataArray[x]); 
+		}
+		return edgeData;
+	}
 }
-
-
